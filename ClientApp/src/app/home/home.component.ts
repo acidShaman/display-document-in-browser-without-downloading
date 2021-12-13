151ff0ce9407c077eebfa73f5cb0314e7997fa9e
andrew.motruk <andrew.m@code-care.pro>
2021-12-13 13:37:04 +0200

    Implemented 4 out of 8 annotation tools

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { fabric } from 'fabric';
+import Annotation from '../interfaces/annotation';
 
 @Component({
   selector: 'app-home',
@@ -12,16 +13,19 @@ export class HomeComponent implements OnInit {
   protected _canvas?: fabric.Canvas;
   image?: fabric.Image;
 
-
   constructor(@Inject('BASE_URL') baseUrl: string, sanitizer: DomSanitizer) {
     const url = sanitizer.bypassSecurityTrustResourceUrl(baseUrl + 'page');
     this.page = new Page(url);
-    console.log("IMG LINK", this.page.src)
-    fabric.Image.fromURL((this.page.src as any).changingThisBreaksApplicationSecurity as string, (oImg) => {
+    this.renderPageOnCanvas((this.page.src as any).changingThisBreaksApplicationSecurity as string);
+  }
+
+  ngOnInit() {}
+
+  renderPageOnCanvas(url: string) {
+    fabric.Image.fromURL(url, (oImg) => {
       oImg.scaleX = 0.5;
       oImg.scaleY = 0.5;
       this.image = oImg;
-      console.log("IMAGE", this.image)
       this._canvas = new fabric.Canvas('fabricSurface', {
         selection: false,
         preserveObjectStacking: true,
@@ -33,12 +37,126 @@ export class HomeComponent implements OnInit {
         this.image,
         this._canvas.renderAll.bind(this._canvas)
       );
+      // this.addRectangle();
+      // this.addArrow();
+      // this.addPolygon();
+      // this.addPolyline();
       this._canvas.renderAll();
     })
   }
 
-  ngOnInit() {
+  addPolyline() { //which is freehand
+    const points = [ 
+      {x: 0, y: 0},
+      {x: 200, y: 250},
+      {x: 300, y: 300},
+      {x: 350, y: 100},
+    ];
+    const polyline = new fabric.Polyline(points,{
+      fill: 'transparent',
+      stroke: 'red', //border
+      strokeWidth: 5,
+      left: 0,
+      top: 0
+    })
+    this._canvas.add(polyline)
+  }
 
+  addPolygon() {
+    const points = [ 
+      {x: 0, y: 0},
+      {x: 200, y: 250},
+      {x: 300, y: 300},
+      {x: 350, y: 100},
+    ];
+    const polygon = new fabric.Polygon(points,{
+      fill: 'red', //background
+      stroke: 'black', //border
+      strokeWidth: 1,
+    })
+    this._canvas.add(polygon)
+  }
+
+  addRectangle() {
+    const rect = new fabric.Rect({
+      top: 0,
+      left: 0,
+      width: 200,
+      height: 200,
+      opacity: 0.5,
+      fill: "#000000"
+    })
+    this._canvas.add(rect);
+  }
+
+  addArrow() {
+    let {fromy, fromx, toy, tox} = {
+      fromy: 0,
+      fromx: 0,
+      toy: 200,
+      tox: 200
+    }
+    const angle = Math.atan2(toy - fromy, tox - fromx);
+    const headlen = 10;  // arrow head size
+    const circleRadius = 8;
+  
+    // bring the line end back some to account for arrow head.
+    tox = tox - (headlen) * Math.cos(angle);
+    toy = toy - (headlen) * Math.sin(angle);
+    fromx = fromx + circleRadius * 1.75;
+    fromy = fromy + circleRadius * 1.75;
+  
+    // calculate the points.
+    const points = [
+      {
+        x: fromx,  // start point
+        y: fromy
+      }, {
+        x: fromx - (headlen / 4) * Math.cos(angle - Math.PI / 2), 
+        y: fromy - (headlen / 4) * Math.sin(angle - Math.PI / 2)
+      },{
+        x: tox - (headlen / 4) * Math.cos(angle - Math.PI / 2), 
+        y: toy - (headlen / 4) * Math.sin(angle - Math.PI / 2)
+      }, {
+        x: tox - (headlen) * Math.cos(angle - Math.PI / 2),
+        y: toy - (headlen) * Math.sin(angle - Math.PI / 2)
+      },{
+        x: tox + (headlen) * Math.cos(angle),  // tip
+        y: toy + (headlen) * Math.sin(angle)
+      }, {
+        x: tox - (headlen) * Math.cos(angle + Math.PI / 2),
+        y: toy - (headlen) * Math.sin(angle + Math.PI / 2)
+      }, {
+        x: tox - (headlen / 4) * Math.cos(angle + Math.PI / 2),
+        y: toy - (headlen / 4) * Math.sin(angle + Math.PI / 2)
+      }, {
+        x: fromx - (headlen / 4) * Math.cos(angle + Math.PI / 2),
+        y: fromy - (headlen / 4) * Math.sin(angle + Math.PI / 2)
+      },{
+        x: fromx,
+        y: fromy
+      }
+    ];
+  
+    const pline = new fabric.Polyline(points, {
+      fill: 'white',
+      stroke: 'black',
+      opacity: 1,
+      strokeWidth: 2,
+      originX: 'left',
+      originY: 'top',
+    });
+    const circle = new fabric.Circle({
+      radius: circleRadius,
+      top: 1,
+      left: 1,
+      fill: 'white',
+      stroke: 'black',
+      opacity: 1,
+      strokeWidth: 2,
+    })
+    const arrowLineGroup = new fabric.Group([pline, circle])
+    this._canvas.add(arrowLineGroup);
   }
 }
 
--- ClientApp/src/app/interfaces/annotation.ts
+++ ClientApp/src/app/interfaces/annotation.ts
@@ -1,34 +1,34 @@
 export default interface Annotation {
-  type: string;
-  bounding: number[];
-  weight: number;
-  fill: "solid" | "none";
-  zorder: 1;
-  color: string;
-  background: string;
-  opacity: string; // "0.0" to "1.0"
-  border: Border;
-  font: Font;
-  start: LineNode;
-  end: LineNode;
-  points: Coordinates[]
+  type?: string;
+  bounding?: number[];
+  weight?: number;
+  fill?: "solid" | "none";
+  zorder?: 1;
+  color?: string;
+  background?: string;
+  opacity?: string; // "0.0" to "1.0"
+  border?: Border;
+  font?: Font;
+  start?: LineNode;
+  end?: LineNode;
+  points?: Coordinates[]
 }
 
 export interface Border {
-  style: string;
-  size: number;
+  style?: string;
+  size?: number;
 }
 
 export interface Coordinates {
-  x: number;
-  y: number;
+  x?: number;
+  y?: number;
 }
 
 export interface LineNode {
-  type: string;
-  fill: string;
-  size: number;
-  points: Coordinates;
+  type?: string;
+  fill?: string;
+  size?: number;
+  points?: Coordinates;
 }
 
 export interface Font {
